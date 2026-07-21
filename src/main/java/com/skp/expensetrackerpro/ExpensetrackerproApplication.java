0753a1ff0e90c6ef336c6c89cb46d8a5c8364970
skype-28 <skype28.business@gmail.com>
2026-07-21 23:23:46 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/skp/expensetrackerpro/ExpensetrackerproApplication.java
@@ -0,0 +1,13 @@
+package com.skp.expensetrackerpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensetrackerproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skp/expensetrackerpro/ExpensetrackerproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skp.expensetrackerpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
